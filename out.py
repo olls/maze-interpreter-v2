c569c9bb3257459c2c9a3b9885023719015e35b3
Oliver Faircliff <faircliff.oliver@gmail.com>
2015-06-06 20:24:09 +0100

    Removed old output function

--- out.py
+++ out.py
@@ -75,41 +75,6 @@ def log_lines(logs, n, simple_out=False):
     return ('\n' + ('' if simple_out else CLS_END_LN)).join(line for line in logs.split('\n')[-n:])
 
 
-def output(maze, cars, logs, colors=True, simple_out=False):
-    out = ''
-
-    for y, row in enumerate(maze):
-        for x, cell in enumerate(row):
-
-            # Get value of cell without car.
-            value = controls.display[cell.name].format(value=cell.value)
-            color = controls.colors[cell.name]
-
-            # Should only be one car in cell, but if not print last one.
-            car = [car for car in cars if car.x == x and car.y == y]
-            if car:
-                # Replace value of cell with value of car if there is one.
-                value = car[-1].value
-                color = {
-                    'bg': color['bg'],
-                    'fg': WHITE if color['bg'] == BLACK else BLACK,
-                    'style': None
-                }
-
-            # Two characters wide.
-            value = funcs.escape(value)
-            if len(value) > 2:
-                value = value[:2]
-            elif len(value) < 2:
-                value = ('0' * (2 - len(value))) + value
-
-            out += colorStr(value, **color) if colors else value
-        out += ('' if simple_out else CLS_END_LN) + '\n'
-    out += logs
-
-    print(('' if simple_out else REDRAW) + out + ('' if simple_out else CLS_END))
-
-
 def init():
     print(HIDE_CUR + CLS)
 
