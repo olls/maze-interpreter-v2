dfe29d25a56cc210329e31f91d9cad26f83406db
Oliver Faircliff <faircliff.oliver@gmail.com>
2015-06-06 14:19:43 +0100

    Moved output stuff into new file

--- main.py
+++ main.py
@@ -3,19 +3,9 @@ import time
 import re
 import argparse
 
-import funcs
-import controls
 import parser
 import run
-from colors import *
-
-
-CLS = '\033[2J'
-CLS_END = '\033[0J'
-CLS_END_LN = '\033[0K'
-REDRAW = '\033[0;0f'
-HIDE_CUR = '\033[?25l'
-SHOW_CUR = '\033[?25h'
+import out
 
 
 class Car:
@@ -45,45 +35,6 @@ def get_args():
     return args
 
 
-def log_lines(logs, n):
-    return ('\n' + CLS_END_LN).join(line for line in logs.split('\n')[-n:])
-
-
-def output(maze, cars, logs, colors=True):
-    out = ''
-
-    for y, row in enumerate(maze):
-        for x, cell in enumerate(row):
-
-            # Get value of cell without car.
-            value = controls.display[cell.name].format(value=cell.value)
-            color = controls.colors[cell.name]
-
-            # Should only be one car in cell, but if not print last one.
-            car = [car for car in cars if car.x == x and car.y == y]
-            if car:
-                # Replace value of cell with value of car if there is one.
-                value = car[-1].value
-                color = {
-                    'bg': color['bg'],
-                    'fg': WHITE if color['bg'] == BLACK else BLACK,
-                    'style': None
-                }
-
-            # Two characters wide.
-            value = funcs.escape(value)
-            if len(value) > 2:
-                value = value[:2]
-            elif len(value) < 2:
-                value = ('0' * (2 - len(value))) + value
-
-            out += colorStr(value, **color) if colors else value
-        out += CLS_END_LN + '\n'
-    out += logs
-
-    print(REDRAW + out + CLS_END)
-
-
 def main():
     logs = ''
     args = get_args()
@@ -94,8 +45,8 @@ def main():
     cars = run.create_cars(maze, Car)
 
     if args.debug:
-        print(HIDE_CUR + CLS)
-        output(maze, cars, logs, args.no_colors)
+        out.init()
+        out.output(maze, cars, logs, args.no_colors)
 
     try:
         while cars:
@@ -103,7 +54,7 @@ def main():
 
             if args.debug:
                 time.sleep(1 / args.fps)
-                output(maze, cars, logs, args.no_colors)
+                out.output(maze, cars, logs, args.no_colors)
 
             maze, cars, new_logs = run.car_actions(maze, cars, functions, debug=args.debug)
 
@@ -114,7 +65,7 @@ def main():
                 print(new_logs, end='')
 
     finally:
-        if args.debug: print(SHOW_CUR)
+        if args.debug: out.end()
 
 
 if __name__ == '__main__':
---
+++ out.py
@@ -0,0 +1,57 @@
+import controls
+import funcs
+from colors import *
+
+
+CLS = '\033[2J'
+CLS_END = '\033[0J'
+CLS_END_LN = '\033[0K'
+REDRAW = '\033[0;0f'
+HIDE_CUR = '\033[?25l'
+SHOW_CUR = '\033[?25h'
+
+
+def log_lines(logs, n):
+    return ('\n' + CLS_END_LN).join(line for line in logs.split('\n')[-n:])
+
+
+def output(maze, cars, logs, colors=True):
+    out = ''
+
+    for y, row in enumerate(maze):
+        for x, cell in enumerate(row):
+
+            # Get value of cell without car.
+            value = controls.display[cell.name].format(value=cell.value)
+            color = controls.colors[cell.name]
+
+            # Should only be one car in cell, but if not print last one.
+            car = [car for car in cars if car.x == x and car.y == y]
+            if car:
+                # Replace value of cell with value of car if there is one.
+                value = car[-1].value
+                color = {
+                    'bg': color['bg'],
+                    'fg': WHITE if color['bg'] == BLACK else BLACK,
+                    'style': None
+                }
+
+            # Two characters wide.
+            value = funcs.escape(value)
+            if len(value) > 2:
+                value = value[:2]
+            elif len(value) < 2:
+                value = ('0' * (2 - len(value))) + value
+
+            out += colorStr(value, **color) if colors else value
+        out += CLS_END_LN + '\n'
+    out += logs
+
+    print(REDRAW + out + CLS_END)
+
+
+def init():
+    print(HIDE_CUR + CLS)
+
+def end():
+    print(SHOW_CUR)
